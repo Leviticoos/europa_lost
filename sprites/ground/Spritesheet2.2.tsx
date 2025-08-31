d129ac66e0eb2fc32cc84ccbcccb3331971ccc0e
Leviticoos <105832125+Leviticoos@users.noreply.github.com>
2025-08-31 11:33:13 -0600

    Got Tiled Baby
    -Set up tiled to be used as a map editor

---
+++ sprites/ground/Spritesheet2.2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Spritesheet2.2" tilewidth="13" tileheight="13" tilecount="500" columns="50">
+ <image source="Spritesheet2.2.png" width="650" height="130"/>
+</tileset>
