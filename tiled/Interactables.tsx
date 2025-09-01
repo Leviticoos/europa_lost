723b8673b2713e77594ae0dc6e313a773b80d614
ARhoden17 <asa.rhoden@comcast.net>
2025-08-31 19:30:04 -0700

    Groundsheet work
    Continued to cook on ground textures and also test with the tiled world. Uploaded some of the smaller spritesheets as well, including vehicles and interactables

---
+++ tiled/Interactables.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Interactables" tilewidth="13" tileheight="13" tilecount="200" columns="20">
+ <image source="../sprites/Interactables/Interactables.png" width="260" height="130"/>
+</tileset>
